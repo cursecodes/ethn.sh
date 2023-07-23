0fabf01e15a1a7624451b66305289ec41dd2925c
curse <58047023+cursecodes@users.noreply.github.com>
2023-07-23 00:19:32 -0600

    fix images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 import partytown from "@astrojs/partytown";
 import react from "@astrojs/react";
 import cloudflare from "@astrojs/cloudflare";
@@ -9,7 +8,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image(), partytown(), react(), mdx()],
+  integrations: [tailwind(), partytown(), react(), mdx()],
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare(),
 });
